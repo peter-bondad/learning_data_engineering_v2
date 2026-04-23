96d61a12420d37afb5c151187e5386d8634cd42d
peter-bondad <bondadpetermaironne@gmail.com>
2026-04-23 14:31:33 +0800

    feat: refactor get_s3_client to use get_env for environment variable retrieval

--- src/infra/storage/store_bucket.py
+++ src/infra/storage/store_bucket.py
@@ -1,11 +1,15 @@
 import boto3
-import os
-
+from src.utils.get_env import get_env
 
 def get_s3_client():
+    host = get_env("MINIO_HOST")
+    port = get_env("MINIO_API_PORT")
+
+    endpoint = get_env("S3_ENDPOINT", f"http://{host}:{port}")
+
     return boto3.client(
         "s3",
-        endpoint_url=os.getenv("S3_ENDPOINT", "http://localhost:9000"),
-        aws_access_key_id=os.getenv("S3_ACCESS_KEY", "minioadmin"),
-        aws_secret_access_key=os.getenv("S3_SECRET_KEY", "minioadmin"),
+        endpoint_url=endpoint,
+        aws_access_key_id=get_env("S3_ACCESS_KEY"),
+        aws_secret_access_key=get_env("S3_SECRET_KEY"),
     )
