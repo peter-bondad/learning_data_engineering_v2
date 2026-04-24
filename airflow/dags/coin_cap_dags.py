b8d2c95a0b0062f198b8d7bce5d53446e96a35b1
peter-bondad <bondadpetermaironne@gmail.com>
2026-04-24 22:29:53 +0800

    refactor: rename read_task to extract_task and revised improve logging in extract and load tasks

--- airflow/dags/coin_cap_dags.py
+++ airflow/dags/coin_cap_dags.py
@@ -2,9 +2,15 @@ from airflow import DAG
 from airflow.decorators import task
 from datetime import datetime, timedelta
 from airflow.models import Variable
+from src.logging.logger import get_logger
 
 from src.extract.coin_cap import fetch_coin_cap
-from src.infra.storage.client import read_raw_data, upload_raw_data
+from src.infra.storage.client import upload_raw_data
+from src.load.coin_cap import load
+
+import logging
+
+logger = get_logger(__name__)
 
 default_args = {
     "owner": "airflow",
@@ -20,20 +26,25 @@ with DAG(
     schedule="@daily",
     start_date=datetime(2024, 1, 1),
     catchup=False,
-    tags=["etl", "coin_cap"],
+    tags=["elt", "coin_cap"],
 ) as dag:
 
     @task
     def extract_task():
+        logging.getLogger("src.extract.coin_cap").setLevel(logging.DEBUG)
+        logger.info("Starting extract task")
         api_key = Variable.get("COIN_CAP_API_KEY")
         coins = fetch_coin_cap(api_key)
         key = upload_raw_data(coins)
+        logger.info(f"Extract complete — stored at {key}")
         return key
 
     @task
-    def read_task(key):
-        data = read_raw_data(key)
-        print(f"Loaded {len(data)} records from {key}")
+    def load_task(key: str):
+        logging.getLogger("src.load.coin_cap").setLevel(logging.DEBUG)
+        logger.info(f"Starting load task for key: {key}")
+        load(key)
+        logger.info("Load complete")
 
     key = extract_task()
-    read_task(key)
+    load_task(key)
