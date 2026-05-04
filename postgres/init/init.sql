ac8c9cd8c39809b7dc1263d4fd80268f4fd21cf2
peter-bondad <bondadpetermaironne@gmail.com>
2026-05-04 22:31:36 +0800

    feat: update init.sql to remove raw_db creation and enhance comments for clarity

--- postgres/init/init.sql
+++ postgres/init/init.sql
@@ -1,3 +1,4 @@
+-- This SQL script initializes the PostgreSQL database by creating two databases: `airflow_db` for Airflow and `analytics_db` for analytics purposes. 
+-- These databases will be used to store data and manage workflows for data engineering projects.
 CREATE DATABASE airflow_db;
-CREATE DATABASE raw_db;
 CREATE DATABASE analytics_db;
