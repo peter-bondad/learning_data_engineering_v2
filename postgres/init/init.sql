f8f206cef9cac47190e441e4768dc0c35d13a8eb
peter-bondad <bondadpetermaironne@gmail.com>
2026-05-06 20:31:34 +0800

    feat: remove creation of airflow_db from init.sql to streamline database initialization

--- postgres/init/init.sql
+++ postgres/init/init.sql
@@ -1,4 +1,3 @@
--- This SQL script initializes the PostgreSQL database by creating two databases: `airflow_db` for Airflow and `analytics_db` for analytics purposes. 
--- These databases will be used to store data and manage workflows for data engineering projects.
-CREATE DATABASE airflow_db;
+
+DROP DATABASE IF EXISTS analytics_db;
 CREATE DATABASE analytics_db;
